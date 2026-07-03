641e15dac0e09c1d72219c1c8ddb4e07c3997f0e
Vignesh M <132240325+0x0906@users.noreply.github.com>
2026-07-04 01:18:17 +0530

    Delete backend directory

--- backend/src/routes/redirectRoutes.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { redirectUrl } from '../controllers/urlController';
-const router = express.Router();
-router.get('/:shortCode', redirectUrl);
-export default router;
